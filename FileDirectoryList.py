bab27137fdec80b3ba316bef7ec62a30ece97950
antonyhumeniuk <35650197+antonyhumeniuk@users.noreply.github.com>
2019-04-28 11:53:50 +0200

    Update FileDirectoryList.py

--- FileDirectoryList.py
+++ FileDirectoryList.py
@@ -19,39 +19,3 @@ def DirectoryList():
            # ListOfDirs.append(docs)
 
 DirectoryList()
-
-
-
-
-
-
-
-
-            
-            #FilesInDirectory(docs)
-            #print (ListOfDirs)
-            #for name in files:
-            #print(os.path.join(dirs,name))
-            #for dirs in root:
-            #temppath=os.path.join(root, dirs)
-            #os.chdir(os.path.join(root, dirs ))
-            #print("**********",temppath,"*****'*******")
-            #usingdirlist(path)
-            #for name in dirs:
-            #print(os.path.join(temppath,name))
-            #print(" "*10,name)
-              
-
-
-
-
-#for i in range(len(ListOfDirs)):
-    #print(ListOfDirs[i])
-    #os.chdir(ListOfDirs[i]))
-    #onlyfiles = [f for f in ListOfDirs(os.path(i)) if isfile(join(i, f))]
-
-    #onlyfiles
-    
-    
-
-
